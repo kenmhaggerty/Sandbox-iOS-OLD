6ca640c2e70ff8eac4a6fbc8b46523cf2ee750cb
Ken M. Haggerty <eurekavalleyco@gmail.com>
2015-08-10 13:16:23 -0400

    Adds AppInfo.h/.m.

---
+++ Sandbox/AppInfo.h
@@ -0,0 +1,23 @@
+//
+//  AppInfo.h
+//  Sandbox
+//
+//  Created by Ken M. Haggerty on 8/9/15.
+//  Copyright (c) 2015 MCMDI. All rights reserved.
+//
+
+#pragma mark - // NOTES (Public) //
+
+#pragma mark - // IMPORTS (Public) //
+
+#import <Foundation/Foundation.h>
+
+#pragma mark - // PROTOCOLS //
+
+#pragma mark - // DEFINITIONS (Public) //
+
+#define TEXT_LOGIN @"Sign In"
+#define TEXT_LOGOUT @"Sign Out"
+
+@interface AppInfo : NSObject
+@end
---
+++ Sandbox/AppInfo.m
@@ -0,0 +1,38 @@
+//
+//  AppInfo.m
+//  Sandbox
+//
+//  Created by Ken M. Haggerty on 8/9/15.
+//  Copyright (c) 2015 MCMDI. All rights reserved.
+//
+
+#pragma mark - // NOTES (Private) //
+
+#pragma mark - // IMPORTS (Private) //
+
+#import "AppInfo.h"
+#import "AKDebugger.h"
+#import "AKGenerics.h"
+
+#pragma mark - // DEFINITIONS (Private) //
+
+@interface AppInfo ()
+@end
+
+@implementation AppInfo
+
+#pragma mark - // SETTERS AND GETTERS //
+
+#pragma mark - // INITS AND LOADS //
+
+#pragma mark - // PUBLIC METHODS //
+
+#pragma mark - // CATEGORY METHODS //
+
+#pragma mark - // DELEGATED METHODS //
+
+#pragma mark - // OVERWRITTEN METHODS //
+
+#pragma mark - // PRIVATE METHODS //
+
+@end
