22ca90496b4e5b58bbac5e745dd516133e9963e9
Eureka Valley Co <eurekavalleyco@gmail.com>
2015-12-04 10:14:00 -0500

    Adds pragma marks to SandboxAppDelegate.h.

--- Sandbox/SandboxAppDelegate.h
+++ Sandbox/SandboxAppDelegate.h
@@ -6,7 +6,15 @@
 //  Copyright (c) 2012 MCMDI. All rights reserved.
 //
 
+#pragma mark - // NOTES (Public) //
+
+#pragma mark - // IMPORTS (Public) //
+
 #import <UIKit/UIKit.h>
+
+#pragma mark - // PROTOCOLS //
+
+#pragma mark - // DEFINITIONS (Public) //
 
 @interface SandboxAppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
