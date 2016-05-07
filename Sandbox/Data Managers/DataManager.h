282d9ad93361f27ca9cba92340aa4f6fc178253f
kenmhaggerty <kenmhaggerty@gmail.com>
2016-05-07 12:28:10 -0400

    Organizes Xcode directory structure and installs CocoaPods.

---
+++ Sandbox/Data Managers/DataManager.h
@@ -0,0 +1,13 @@
+//
+//  DataManager.h
+//  Sandbox
+//
+//  Created by Ken M. Haggerty on 5/7/16.
+//  Copyright © 2016 MCMDI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataManager : NSObject
+
+@end
---
+++ Sandbox/Data Managers/DataManager.m
@@ -0,0 +1,13 @@
+//
+//  DataManager.m
+//  Sandbox
+//
+//  Created by Ken M. Haggerty on 5/7/16.
+//  Copyright © 2016 MCMDI. All rights reserved.
+//
+
+#import "DataManager.h"
+
+@implementation DataManager
+
+@end
